84a8c620cd10b595d8a65c1e838ff055bafa35ee
diego <diegozaratean@gmail.com>
2023-09-21 01:21:54 +0000

    finish class 1

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,7 +12,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			count: 0,
+			characters: [],
+			loquesea: 'literalment lo que sea'
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useContext } from "react"; // 1. importar el hook de useContext
+import {Context} from '../store/appContext.js' // 2. Importar el contexto de mi applicación
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () =>{
+
+	const {store} = useContext(Context) // 3. activar el useContext y ahcer destructurig de lo que necesito, propiedades o metodos
+	console.log('Demo desde flux')
+	console.log(store)
+	console.log(store.demo)
+	console.log(store.count)
+
+	return(
+		<div className="text-center mt-5">
+			<h1>Hello Rigo!</h1>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<a href="#" className="btn btn-success">
+				If you see this green button, bootstrap is working 123
+			</a>
+		</div>
+	)
+};
